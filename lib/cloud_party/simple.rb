606826f9ea616d497a8deddaf724b232ccfba27c
Ken Spencer <me@iotaspencer.me>
2018-12-26 12:22:27 -0500

    :poop:

---
+++ lib/cloud_party/simple.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'rubyflare'
+require 'app_configuration'
+module CloudParty
+  class Simple
+    def initialize
+      cp_config = AppConfiguration.new('config') do
+        base_local_path Pathname.new(Dir.home).join('.cloud_party/')
+        base_global_path Pathname.new(Etc.sysconfdir).join('cloud_party')
+        use_env_variables true
+        prefix 'cloud_party'
+      end
+      cfcli_config = AppConfiguration.new('config') do
+        base_local_path Pathname.new(Dir.home).join('.cfcli/')
+        base_global_path Pathname.new(Etc.sysconfdir).join('cloudflare_cli')
+        use_env_variables true
+        prefix 'cf_cli'
+      end
+      @email = cp_config.email || cfcli_config.email
+      @api_key = cp_config.api_key || cfcli_config.api_key
+    end
+
+    # @return [String] the email string
+    def email
+      @email
+    end
+
+    # @return [String] the api key string
+    def api_key
+      @api_key
+    end
+
+    def connect
+      Rubyflare.connect_with(email, api_key)
+    end
+  end
+end
