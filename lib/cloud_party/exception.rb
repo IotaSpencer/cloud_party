1b9bcda7ff383f6bfe1ce9f7a4e212d35ea160c9
Ken Spencer <me@iotaspencer.me>
2019-09-14 17:15:12 -0400

    switch keywords to regular arguments

--- lib/cloud_party/exception.rb
+++ lib/cloud_party/exception.rb
@@ -10,25 +10,43 @@ module CloudParty
       end
     end
     class RequestError < StandardError
-      def initialize(obj:, method:, code:, response:, endpoint:)
-        @obj      = obj
+      def initialize(message, method, endpoint, code, body)
+        super(message)
         @method   = method
         @endpoint = endpoint
         @code     = code
-        @response = response
+        @body = body
       end
 
       def to_s
         [error_string.squish, extra_string].join("\n")
       end
 
-      def self.extra_string
+      def extra_string
         # This method should be overridden
       end
 
       # override error_string to provide your own error_string
-      def self.error_string
+      def error_string
         # This method should be overridden
+      end
+    end
+    class InputError < ArgumentError
+      attr_reader :input, :obj
+      # @param [String] message message
+      # @param [Object] obj object
+      # @param [String] input input string
+      def initialize(message, obj, input)
+        super(message)
+        @obj = obj
+        @input = input
+      end
+    end
+    class NoDefinedZoneError < StandardError
+      attr_reader :obj
+      def initialize(message, obj)
+        super(message)
+        @obj = obj
       end
     end
   end
